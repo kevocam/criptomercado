04070b948aabbc339272d361aba06be7f6283848
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-10-22 11:57:28 -0500

    bugs

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -2,20 +2,20 @@
   <header class="shadow w-screen">
     <nav>
       <nav
-        class="flex w-100 items-center justify-between flex-wrap bg-green-400 p-6"
+        class="flex items-center justify-spacearound flex-wrap bg-green-400 p-6"
       >
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <px-icon class="mr-2"></px-icon>
           <router-link
             :to="{ name: 'home' }"
             class="font-semibold text-xl tracking-tight"
-            >PlatziExchange</router-link
+            >CriptoMercado</router-link
           >
         </div>
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
         >
-          <div class="text-sm lg:flex-grow">
+          <div class="text-md lg:flex-grow">
             <router-link
               v-for="l in links"
               :key="l.title"
